ebdcc62171f7aae07af0fc8e51748cce5e132b8f
Katz Ueno <katz@concrete5.co.jp>
2017-06-25 11:21:08 +0900

    - Support to execute the script from different directory - Clean-up the code to make follow PSR-2 (didn't validate)

--- deployments.php
+++ deployments.php
@@ -6,16 +6,6 @@
  *
  */
 
-/**
-* The Server Path to git repository
-*/
-$serverpath = '/path/to/git/repo';
-
-/**
-* The Secret Key
-*/
-$secret_key = 'EnterYourSecretKeyHere';
-
 /*
  *    Webhook Sample
  *        For BitBucket, use 'POST HOOK'
@@ -26,6 +16,16 @@ $secret_key = 'EnterYourSecretKeyHere';
  */
 
 /**
+* The Server Path to git repository
+*/
+$serverpath = '/path/to/git/repo';
+
+/**
+* The Secret Key
+*/
+$secret_key = 'EnterYourSecretKeyHere';
+
+/**
 * The TimeZone format used for logging.
 * @link    http://php.net/manual/en/timezones.php
 */
@@ -33,139 +33,135 @@ date_default_timezone_set('Asia/Tokyo');
 
 class Deploy {
 
-  /**
-  * A callback function to call after the deploy has finished.
-  * 
-  * @var callback
-  */
-  public $post_deploy;
-  
-  /**
-  * The name of the file that will be used for logging deployments. Set to 
-  * FALSE to disable logging.
-  * 
-  * @var string
-  */
-  private $_log = 'deployments.log';
+    /**
+    * The name of the file that will be used for logging deployments. Set to 
+    * FALSE to disable logging.
+    * 
+    * @var string
+    */
+    private $_log = 'deployments.log';
+    
+    /**
+    * The timestamp format used for logging.
+    * 
+    * @link    http://www.php.net/manual/en/function.date.php
+    * @var     string
+    */
+    private $_date_format = 'Y-m-d H:i:sP';
+    
+    /**
+    * The name of the branch to pull from.
+    * 
+    * @var string
+    */
+    private $_branch = 'master';
+    
+    /**
+    * The name of the remote to pull from.
+    * 
+    * @var string
+    */
+    private $_remote = 'origin';
+    
+    /**
+    * If the git contains submodule and want this git auto deploy to sync submodule at the same time.
+    * 
+    * @var boolean
+    */
+    private $_syncSubmodule = false;
+    
+    /**
+    * A callback function to call after the deploy has finished.
+    * 
+    * @var callback
+    */
+    public $post_deploy;
+    
+    /**
+    * The directory where your website and git repository are located, can be 
+    * a relative or absolute path
+    * 
+    * @var string
+    */
+    private $_directory;
 
-  /**
-  * The timestamp format used for logging.
-  * 
-  * @link    http://www.php.net/manual/en/function.date.php
-  * @var     string
-  */
-  private $_date_format = 'Y-m-d H:i:sP';
+    /**
+    * Sets up defaults.
+    * 
+    * @param  string  $directory  Directory where your website is located
+    * @param  array   $options    Options for default settings
+    */
+    public function __construct($directory, $options = array())
+    {
+        // Determine the directory path
+        $this->_directory = realpath($directory).DIRECTORY_SEPARATOR;
+    
+        $available_options = array('log', 'date_format', 'branch', 'remote');
+    
+        foreach ($options as $option => $value){
+            if (in_array($option, $available_options)) {
+                $this->{'_'.$option} = $value;
+            }
+        }
+    
+        $this->log('Attempting deployment...');
+        $this->log('Directory:' . $this->_directory);
+    }
 
-  /**
-  * The name of the branch to pull from.
-  * 
-  * @var string
-  */
-  private $_branch = 'master';
+    /**
+    * Writes a message to the log file.
+    * 
+    * @param  string  $message  The message to write
+    * @param  string  $type     The type of log message (e.g. INFO, DEBUG, ERROR, etc.)
+    */
+    public function log($message, $type = 'INFO')
+    {
+        if ($this->_log)
+        {
+            // Set the name of the log file
+            $filename = $this->_log;
+    
+            if ( ! file_exists($filename))
+            {
+                // Create the log file
+                file_put_contents($filename, '');
+    
+                // Allow anyone to write to log files
+                chmod($filename, 0666);
+            }
+    
+            // Write the message into the log file
+            // Format: time --- type: message
+            file_put_contents($filename, date($this->_date_format).' --- '.$type.': '.$message.PHP_EOL, FILE_APPEND);
+        }
+    }
 
-  /**
-  * The name of the remote to pull from.
-  * 
-  * @var string
-  */
-  private $_remote = 'origin';
-
-  /**
-  * If the git contains submodule and want this git auto deploy to sync submodule at the same time.
-  * 
-  * @var boolean
-  */
-  private $_syncSubmodule = false;
-
-  /**
-  * The directory where your website and git repository are located, can be 
-  * a relative or absolute path
-  * 
-  * @var string
-  */
-  private $_directory;
-
-  /**
-  * Sets up defaults.
-  * 
-  * @param  string  $directory  Directory where your website is located
-  * @param  array   $data       Information about the deployment
-  */
-  public function __construct($directory, $options = array())
-  {
-      // Determine the directory path
-      $this->_directory = realpath($directory).DIRECTORY_SEPARATOR;
-
-      $available_options = array('log', 'date_format', 'branch', 'remote');
-
-      foreach ($options as $option => $value)
-      {
-          if (in_array($option, $available_options))
-          {
-              $this->{'_'.$option} = $value;
-          }
-      }
-
-      $this->log('Attempting deployment...');
-  }
-
-  /**
-  * Writes a message to the log file.
-  * 
-  * @param  string  $message  The message to write
-  * @param  string  $type     The type of log message (e.g. INFO, DEBUG, ERROR, etc.)
-  */
-  public function log($message, $type = 'INFO')
-  {
-      if ($this->_log)
-      {
-          // Set the name of the log file
-          $filename = $this->_log;
-
-          if ( ! file_exists($filename))
-          {
-              // Create the log file
-              file_put_contents($filename, '');
-
-              // Allow anyone to write to log files
-              chmod($filename, 0666);
-          }
-
-          // Write the message into the log file
-          // Format: time --- type: message
-          file_put_contents($filename, date($this->_date_format).' --- '.$type.': '.$message.PHP_EOL, FILE_APPEND);
-      }
-  }
-
-  /**
-  * Executes the necessary commands to deploy the website.
-  */
-  public function execute()
-  {
-      try
-      {
-          // Git Submodule - Measure the execution time
-          $strtedAt = microtime(true);
-          // Make sure we're in the right directory
-          chdir($this->_directory);
-          $this->log('Changing working directory... ');
-
-          // Discard any changes to tracked files since our last deploy
-          exec('git reset --hard HEAD', $output);
-          if (is_array($output)) {
-              $output = implode(' ', $output);
-          }
-          $this->log('Reseting repository... '.$output);
-
-          // Update the local repository
-          $output = '';
-          exec('git pull '.$this->_remote.' '.$this->_branch, $output);
-          if (is_array($output)) {
-              $output = implode(' ', $output);
-          }
-          $this->log('Pulling in changes... '.$output);
-
-          if ($this->_syncSubmodule) {
+    /**
+    * Executes the necessary commands to deploy the website.
+    */
+    public function execute()
+    {
+        try
+        {
+            // Git Submodule - Measure the execution time
+            $strtedAt = microtime(true);
+            
+            // Discard any changes to tracked files since our last deploy
+            exec('git --git-dir='.$this->_directory.'/.git --work-tree='.$this->_directory . ' reset --hard HEAD', $output);
+            if (is_array($output)) {
+                $output = implode(' ', $output);
+            }
+            $this->log('Reseting repository... '.$output);
+            
+            // Update the local repository
+            $output = '';
+            exec('git --git-dir='.$this->_directory.'/.git --work-tree='.$this->_directory . ' pull', $output);
+            if (is_array($output)) {
+                $output = implode(' ', $output);
+            }
+            $this->log('Pulling in changes... '.$output);
+            
+            if ($this->_syncSubmodule) {
             // Wait 2 seconds if main git pull takes less than 2 seconds.
             $endedAt = microtime(true);
             $mDuration = $endedAt - $strtedAt;
@@ -175,30 +171,29 @@ class Deploy {
             }
             // Update the submodule
             $output = '';
-            exec('git submodule update --init --recursive --remote', $output);
+            exec('git --git-dir='.$this->_directory.'/.git --work-tree='.$this->_directory . ' submodule update --init --recursive --remote', $output);
             if (is_array($output)) {
                 $output = implode(' ', $output);
             }
-            $this->log('Updating submodules...'.$output);
-          }
-
-          // Secure the .git directory
-          exec('chmod -R og-rx .git');
-          $this->log('Securing .git directory... ');
-
-          if (is_callable($this->post_deploy))
-          {
-              call_user_func($this->post_deploy, $this->_data);
-          }
-
-          $this->log('Deployment successful.');
-      }
-      catch (Exception $e)
-      {
-          $this->log($e, 'ERROR');
-      }
-  }
-
+                $this->log('Updating submodules...'.$output);
+            }
+            
+            // Secure the .git directory
+            exec('chmod -R og-rx . ' .$this->_directory .'/.git');
+            $this->log('Securing .git directory... ');
+            
+            if (is_callable($this->post_deploy)) {
+                  call_user_func($this->post_deploy, $this->_data);
+            }
+            
+            $this->log('Deployment successful.');
+        }
+            catch (Exception $e)
+        {
+            $this->log($e, 'ERROR');
+        }
+    }
+    
 }
 
 $deploy = new Deploy($serverpath);
